36053bca8922fc18346a5222a86f675371528576
Vitaliy Paykov <vitaliy.paykov@gmail.com>
2021-02-04 22:51:53 +0200

    Better structure and ts support

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import { PORT } from '@config';
 import express from 'express';
-import { PORT } from './config';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Express Server'));
+app.get('/', (req, res) => res.send('Express Server!'));
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
